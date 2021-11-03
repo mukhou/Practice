a94cf9f33d24072d0a60a064e3fc60b16d2bea10
jhini <banerjeeurmi@gmail.com>
2021-11-03 08:07:34 -0400

    From Java 8 In Action

---
+++ src/main/java/java8/lambda/java8inaction/Apple.java
@@ -0,0 +1,24 @@
+/* 
+User: Urmi
+Date: 11/3/2021 
+Time: 8:01 AM
+*/
+
+package java8.lambda.java8inaction;
+
+public class Apple {
+
+    private int weight;
+
+    public Apple(int weight) {
+        this.weight = weight;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+}
