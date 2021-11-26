6168a983b94c91e8351d6494d962d8101db7a06c
mukhou <banerjeeurmi@gmail.com>
2021-11-25 23:50:32 -0500

    minor change

--- src/main/java/dynamicprog/LongestCommonSubsequenceInTwoStrings.java
+++ src/main/java/dynamicprog/LongestCommonSubsequenceInTwoStrings.java
@@ -5,17 +5,15 @@ Time: 5:04 PM
 */
 
 /**
- * Given two sequences, find the length of longest subsequence present in both of them.
+ * Given two sequences, find the length of the longest subsequence present in both of them.
  * A subsequence is a sequence that appears in the same relative order, but not necessarily contiguous.
  * For example, “abc”, “abg”, “bdf”, “aeg”, ‘”acefg”, .. etc are subsequences of “abcdefg”.
  */
 package dynamicprog;
 
-import java.util.Arrays;
-
 //https://www.youtube.com/watch?v=ASoaQq66foQ
 //https://www.geeksforgeeks.org/longest-common-subsequence-dp-4/
-//https://github.com/bephrem1/backtobackswe/blob/master/Dynamic%20Programming%2C%20Recursion%2C%20%26%20Backtracking/LongestCommonSubsequence/LongestCommonSubsequence.java
+//https://github.com/bephrem1/backtobackswe/tree/master/Dynamic%20Programming%2C%20Recursion%2C%20%26%20Backtracking/LongestCommonSubsequence
 public class LongestCommonSubsequenceInTwoStrings {
     private static int longestCommonSubsequence(String s1, String s2, int m, int n) {
 
@@ -26,11 +24,12 @@ public class LongestCommonSubsequenceInTwoStrings {
             for(int j = 0; j <= n; j++){
                 if(i == 0 || j == 0){
                     maxLengthTable[i][j] = 0;
-                }else if(s1.charAt(i - 1) == s2.charAt(j - 1)){//IMP: check equality of chars at prev index
+                } else if(s1.charAt(i - 1) == s2.charAt(j - 1)){//IMP: check equality of chars at prev index, because
+                    //maxLengthTable[i][j] contains length of LCS of s1[0..i-1] and s2[0..j-1]
                     /*Characters match. Remove both and lengthen the best answer at the subproblem
-                     * without either final character. That is the answer to this subproblem.*/
+                     * without either final character. That is the answer to this subproblem.
                     maxLengthTable[i][j] = maxLengthTable[i - 1][j - 1] + 1;
-                }else {
+                } else {
                     /**Character mismatch. No +1 happens, we can't lengthen the lcs. But we do need
                      * compete subproblems to find the answer to this one.*/
                     maxLengthTable[i][j] = Math.max(maxLengthTable[i - 1][j], maxLengthTable[i][j - 1]);
@@ -38,6 +37,7 @@ public class LongestCommonSubsequenceInTwoStrings {
             }
         }
 
+        //return the value present at the lengths of both strings
         return maxLengthTable[m][n];
     }
 
@@ -45,9 +45,9 @@ public class LongestCommonSubsequenceInTwoStrings {
     private static int longestCommonSubsequence1(String s1, String s2, int m, int n){
         if(m == 0 || n == 0){
             return 0;
-        }else if(s1.charAt(m - 1) == s2.charAt(n - 1)){
+        } else if(s1.charAt(m - 1) == s2.charAt(n - 1)){
             return 1 + longestCommonSubsequence1(s1, s2, m - 1, n - 1);
-        }else {
+        } else {
             return Math.max(longestCommonSubsequence1(s1, s2, m - 1, n), longestCommonSubsequence1(s1, s2, m, n - 1));
         }
     }
