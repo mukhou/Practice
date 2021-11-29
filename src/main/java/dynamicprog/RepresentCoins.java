478b75b2eab9ac93ce13af35b79f74d8cb78e177
mukhou <banerjeeurmi@gmail.com>
2021-11-29 07:02:28 -0500

    minor change

--- src/main/java/dynamicprog/RepresentCoins.java
+++ src/main/java/dynamicprog/RepresentCoins.java
@@ -1,6 +1,6 @@
 /**
  * Given an infinite number of quarters (25 cents), dimes (10 cents), nickels (5 cents), and
- * pennies (1 cent), write code to calculate the number of ways of representing n cents.
+ * pennies (1 cent), write code to calculate the NUMBER of WAYS of representing n cents.
  */
 package dynamicprog;
 
